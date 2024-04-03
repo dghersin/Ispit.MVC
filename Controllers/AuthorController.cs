0c58f07e03dfae0c008e468055fd35a212033cc4
dghersin <dghersin@gmail.com>
2024-04-03 18:53:49 +0200

    Add project files.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,29 @@
+﻿using Ispit.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ispit.MVC.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Author> authors = new List<Author>();
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                authors.Add(new Author
+                {
+                    ID = i,
+                    Name = $"AuthorName_{i}",
+                    Age = random.Next(20, 60),
+                    Bio = $"Bio_{i}"
+                });
+            }
+
+
+
+            return View("Index", authors);
+        }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Ispit.MVC.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
